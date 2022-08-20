8658be646821f48a1303c295400d86f7de1caa5e
delphox60 <delphox60@gmail.com>
2022-08-20 16:04:06 +0900

    chore: 백건 튜닝 및 시간 조정

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ import pi
 
 pygame.init()
 
+INTERVAL = 0.3
+DEFAULT_OFFSET = 25
+
+stair_nums = [25, 27, 29, 30, 32, 34, 36, 37, 39]
+
+def get_stair_num(n):
+    
+    return stair_nums[n - 1]
+
+
 def play(num):
-    pygame.mixer.Sound('sound/' + str(num + 25) + '.wav').play()
-    time.sleep(0.7)
+    pygame.mixer.Sound('sound/' + str(get_stair_num(num)) + '.wav').play()
+    time.sleep(INTERVAL)
 
 for digit in pi.PI:
     
     if digit == ".":
+        continue
+    
+    if digit == "0" or digit == "9":
+        # time.sleep(DEFAULT_OFFSET)
         continue
     
     play(int(digit))
